b5c20dd526cd1b5ae7e26b6ec57501f452e8f66e
Alysty <pppjw@protonmail.ch>
2021-05-27 08:22:26 -0300

    Created basic component for listing products

---
+++ angular-frontend/src/app/list-product/list-product.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ListProductComponent } from './list-product.component';
+
+
+
+@NgModule({
+  declarations: [
+    ListProductComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ListProductModule { }
