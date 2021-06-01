189694a9bf225eb4ec18bb0e898cf4bface79e5c
Alysty <pppjw@protonmail.ch>
2021-06-01 08:01:27 -0300

    Created the code for ProductGetController

--- spring_boot_backend/src/main/java/com/spring_boot_backend/product/adapter/out/web/ProductGetController.java
+++ spring_boot_backend/src/main/java/com/spring_boot_backend/product/adapter/out/web/ProductGetController.java
@@ -1,4 +1,29 @@
 package com.spring_boot_backend.product.adapter.out.web;
 
+import com.spring_boot_backend.product.application.services.QueryForProductsService;
+import com.spring_boot_backend.product.domain.Product;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Objects;
+
+@RestController
+@RequestMapping(value = "/products")
 public class ProductGetController {
+
+    QueryForProductsService queryForProductsService;
+    public ProductGetController(QueryForProductsService queryForProductsService){
+        Objects.requireNonNull(queryForProductsService);
+        this.queryForProductsService = queryForProductsService;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<Product>> findAll(){
+        List<Product> productList = queryForProductsService.searchForProductsWithNameOnly("");
+        return ResponseEntity.ok().body(productList);
+
+    }
 }
