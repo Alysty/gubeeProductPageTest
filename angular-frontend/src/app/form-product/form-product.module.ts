78e24404c5942578ab6fe7ead042f74b5d9b3376
Alysty <pppjw@protonmail.ch>
2021-06-04 13:42:45 -0300

    Created product form component and made the UI for list product better

---
+++ angular-frontend/src/app/form-product/form-product.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormProductComponent } from './form-product.component';
+
+
+
+@NgModule({
+  declarations: [
+    FormProductComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class FormProductModule { }
