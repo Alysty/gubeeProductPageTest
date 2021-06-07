6648c2497d3945d0f9dfa225df05b1c2a6f65ec8
Alysty <pppjw@protonmail.ch>
2021-06-07 11:12:03 -0300

    Edited Product have the same variable names as the database for easier use

--- angular-frontend/src/app/shared/data-types/Product.ts
+++ angular-frontend/src/app/shared/data-types/Product.ts
@@ -2,59 +2,21 @@ import {Optional} from "@angular/core";
 
 export class Product{
 
-  private _id?: string;
-  private _productName: string;
-  private _description: string;
-  private _targetMarketStack: string[];
-  private _technologiesStack: string[];
+  public id?: string;
+  public productName: string;
+  public description: string;
+  public targetMarketStack: string[];
+  public technologiesStack: string[];
 
 
   constructor( id: string | undefined, productName: string, description: string, targetMarketStack: string[], technologiesStack: string[]) {
-    this._id = id;
-    this._productName = productName;
-    this._description = description;
-    this._targetMarketStack = targetMarketStack;
-    this._technologiesStack = technologiesStack;
-  }
-  get id(): string | undefined {
-    return this._id;
+    this.id = id;
+    this.productName = productName;
+    this.description = description;
+    this.targetMarketStack = targetMarketStack;
+    this.technologiesStack = technologiesStack;
   }
 
-  set id(value: string | undefined) {
-    this._id = value;
-  }
-
-  get productName(): string {
-    return this._productName;
-  }
-
-  set productName(value: string) {
-    this._productName = value;
-  }
-
-  get description(): string {
-    return this._description;
-  }
-
-  set description(value: string) {
-    this._description = value;
-  }
-
-  get targetMarketStack(): string[] {
-    return this._targetMarketStack;
-  }
-
-  set targetMarketStack(value: string[]) {
-    this._targetMarketStack = value;
-  }
-
-  get technologiesStack(): string[] {
-    return this._technologiesStack;
-  }
-
-  set technologiesStack(value: string[]) {
-    this._technologiesStack = value;
-  }
   public static createProductObject(id: string | undefined, productName: string, description: string, targetMarketStack: string, technologiesStack: string) : Product{
     let targetMarketStackTemp = targetMarketStack.split(',');
     let technologiesStackTemp = technologiesStack.split(',');
