54960921aaf6d1dcf68a3c7d111d0050f13f6216
Alysty <pppjw@protonmail.ch>
2021-06-07 08:14:54 -0300

    removed unused import in routing file for list product

--- angular-frontend/src/app/list-product/list-product-routing.module.ts
+++ angular-frontend/src/app/list-product/list-product-routing.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import {ListProductComponent} from "./list-product.component";
 import {RouterModule, Routes} from "@angular/router";
 
